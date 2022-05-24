dd239183dc1d9396b0c0b3f20c0770cb5528574b
poncho-ajmv <alfonsojmoragav@gmail.com>
2022-05-24 16:28:32 -0600

    Se agregaron opadoresaritmericos, opradoreslogicos (v0) y variables.

---
+++ operadoresaritmeticos.py
@@ -0,0 +1,40 @@
+#Son todos los ejercios basicos que se utilizan en la aritmerica basica
+# Suma = +
+# Resta = -
+# Multiplicacion = *
+# Division = /
+# Division entera = // 
+# Potenica = **
+# Residuo de una divison entera = %
+# Igual ==
+
+dato1 = 5
+dato2 = 4
+
+print("La suma de los datos es: ", dato1 + dato2)
+
+
+print("\nLa resta de los datos es: ", dato1 - dato2)
+
+
+print("\nLa multiplica de los datos es: ", dato1 * dato2)
+
+print("\nLa divide de los datos es: ", dato1 / dato2)
+
+print("\nLa division entera de los datos es: ", dato1 // dato2)
+
+print("\nLa Residuo de la division entera de los datos es: ", dato1 % dato2)
+
+print("\nLa potenica de los datos es: ", dato1 ** dato2)
+print("")
+
+#Suma de caracteres 
+'''
+>>>"Hola"+"Mundo" 
+"HolaMUndo"qwQ
+
+>>> "Hola"*3
+'HolaHolaHola'
+'''
+#Se puede tomar combinar varios tipos, tomar en cuneta que no todos se pueden combinar se deben de 
+#hacer con el signo de + o la , para poder juntarlo. 
---
+++ variables.py
@@ -0,0 +1,13 @@
+#Que es una variable en python
+'''Es una peque;a caja donde se almacena un dato. '''
+#Se debe de tomar en cuneta que si afecta si no lleva comilla
+#Con comillas texto
+#SIn comillas digitos
+nombre = "Alfonso"
+apellido = "Drake"
+anios = "19"
+
+print(nombre +" " + apellido +" " + anios)
+
+Lista = [1, 2, 3]
+print(Lista)
