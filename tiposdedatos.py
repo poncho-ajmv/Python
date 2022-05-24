b46293f06cd1d15243a7b47ed6018d6363120b52
poncho-ajmv <alfonsojmoragav@gmail.com>
2022-05-24 15:44:38 -0600

    Agregado tipo de datos

---
+++ tiposdedatos.py
@@ -0,0 +1,67 @@
+'''Tipos de datos en python'''
+#En este tema se  debe de profuncizar mas, se pueden observar diferentes archivos sobre tipos de datos mas avanzados.
+
+'''
+En python internamente categoriza el tipo de dato , como en otro lenguaje si es float, int, etc.
+>>> 3
+3
+
+>>>> 3.14
+3.14
+
+#Las comillas indican que es un string de otros lenguajes
+>>>> 'hola'
+'hola'
+
+>>>> "hola"
+'hola'
+
+>>>> '3'
+'3'
+
+#Boleanos.
+#1
+>>>>True
+True
+#0
+>>>>False
+False
+
+#Coleccion de datos 
+#Es para almacenar una gran cantidad de datos.
+
+#Lista
+#Puede ser del mismo tipo o de distinto.
+>>>>[1,2,3]
+[1,2,3]
+
+>>> [1, "2", "as"]
+[1, '2', 'as']
+
+#Tupla
+#Agrupado los datos entre parenteis
+
+>>>>(4,5,6,)
+(4,5,6,)
+
+#Colecciones de datos
+#No puede haber elementos repetidos 
+{1,2,3,3,4}
+>>>>{1,2,3,3,4}
+
+
+#Diccionarios
+{1:'uno',2:'dos',3:'tres'}
+
+
+
+
+
+
+
+
+
+
+
+
+'''
